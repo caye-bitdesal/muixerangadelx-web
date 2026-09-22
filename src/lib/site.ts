837faef5e9cb630998fa6c93e31572fdcf59fc8a
Cayetano Ruiz <caye@bitdesal.com>
2026-09-22 16:29:26 +0200

    care: improved contact section

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -27,7 +27,9 @@ export const EVENTS_API =
 export const FORMSPREE_FORM_URL =
   import.meta.env.PUBLIC_FORMSPREE_FORM_URL ?? 'https://formspree.io/f/xppwdkyv';
 
-export const CONTACT_ADDRESS = 'C/ Sant Jordi, 2, Elx';
+export const CONTACT_ADDRESS = "Casal d'ACPV/Elx - Jaume I: C/ Sant Jordi, 2, Elx";
+export const CONTACT_MAP_URL =
+  'https://www.google.com/maps/search/?api=1&query=Casal+Jaume+I,+Carrer+Sant+Jordi+2,+Elx';
 export const CONTACT_EMAIL = 'contacte@muixerangadelx.com';
 export const MAP_EMBED_URL =
   'https://maps.google.com/maps?q=Carrer%20Sant%20Jordi%202%2C%20Elx&t=&z=16&ie=UTF8&iwloc=&output=embed';
